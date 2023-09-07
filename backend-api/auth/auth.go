2aa2cde98e122ddfceda918028bae85fe5090fc3
Muhil18 <muhil01803@gmail.com>
2023-09-07 22:42:04 +0530

    added jwt authentication api

--- backend-api/auth/auth.go
+++ backend-api/auth/auth.go
@@ -1,20 +1,68 @@
 package auth
 
 import (
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
 	"fmt"
 	"os"
 
 	"github.com/golang-jwt/jwt"
 )
 
-var hmacSecret []byte = []byte(os.Getenv("Secret_Key"))
+func ParsePrivateKeyFromString(privateKeyPEM string) (interface{}, error) {
+	// Decode the PEM-encoded private key
+	block, _ := pem.Decode([]byte(privateKeyPEM))
+	if block == nil {
+		return nil, errors.New("failed to decode PEM block containing private key")
+	}
+
+	// Parse the RSA private key from the DER data
+	privateKey, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return privateKey, nil
+}
+
+func ParsePublicKeyFromString(publicKeyPEM string) (*rsa.PublicKey, error) {
+	// Decode the PEM-encoded public key
+	block, _ := pem.Decode([]byte(publicKeyPEM))
+	if block == nil {
+		return nil, errors.New("failed to decode PEM block containing public key")
+	}
+
+	// Parse the RSA public key from the DER data
+	pubKey, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	rsaPubKey, ok := pubKey.(*rsa.PublicKey)
+	if !ok {
+		return nil, errors.New("parsed key is not an RSA public key")
+	}
+
+	return rsaPubKey, nil
+}
 
 func GenerateToken(claims map[string]interface{}) string {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims(claims))
+
+	privateKey := fmt.Sprintf("-----BEGIN PRIVATE KEY-----\n%s\n-----END PRIVATE KEY-----", os.Getenv("RSA_Private_Secret_Key"))
+
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims(claims))
 
 	// Sign and get the complete encoded token as a string using the secret
-	tokenString, err := token.SignedString(hmacSecret)
+	pk, err := ParsePrivateKeyFromString(privateKey)
 	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	tokenString, err := token.SignedString(pk)
+	if err != nil {
+		fmt.Println(err)
 		return ""
 	}
 
@@ -22,15 +70,16 @@ func GenerateToken(claims map[string]interface{}) string {
 }
 
 func ValidateToken(tokenString string) map[string]interface{} {
+	publicKey := fmt.Sprintf("-----BEGIN PUBLIC KEY-----\n%s\n-----END PUBLIC KEY-----", os.Getenv("RSA_Public_Secret_Key"))
+
 	var finalClaims = make(map[string]interface{})
+	rsaPublicSecret, err := ParsePublicKeyFromString(publicKey)
+	if err != nil {
+		fmt.Println(err)
+		return finalClaims
+	}
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-		}
-
-		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
-		return hmacSecret, nil
+		return rsaPublicSecret, nil
 	})
 
 	if err != nil {
--- backend-api/azcosmos-api/azcosmos-api.go
+++ backend-api/azcosmos-api/azcosmos-api.go
@@ -4,56 +4,12 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/data/azcosmos"
 )
-
-func GetUser() {
-	endpoint := os.Getenv("Cosmos_DB_Endpoint")
-	key := os.Getenv("Cosmos_DB_Key")
-
-	cred, err := azcosmos.NewKeyCredential(key)
-	if err != nil {
-		log.Fatal("Failed to create a credential: ", err)
-	}
-
-	// Create a CosmosDB client
-	client, err := azcosmos.NewClientWithKey(endpoint, cred, nil)
-	if err != nil {
-		log.Fatal("Failed to create Azure Cosmos DB client: ", err)
-	}
-
-	container, _ := client.NewContainer("DIM", "On-Behalf")
-	pk := azcosmos.NewPartitionKeyNumber(1)
-
-	queryPager := container.NewQueryItemsPager("SELECT c['On-Behalf'] FROM c", pk, nil)
-	var result []map[string]interface{}
-
-	fmt.Printf("queryPager: %v\n", queryPager)
-
-	for queryPager.More() {
-		queryResponse, err := queryPager.NextPage(context.Background())
-		if err != nil {
-			var responseErr *azcore.ResponseError
-			errors.As(err, &responseErr)
-			log.Fatal(err)
-		}
-
-		for _, item := range queryResponse.Items {
-			var itemResponseBody map[string]interface{}
-			err = json.Unmarshal(item, &itemResponseBody)
-			result = append(result, itemResponseBody)
-			if err != nil {
-				panic(err)
-			}
-		}
-	}
-	fmt.Print(result)
-}
 
 func ExecuteQuery(databaseid string, containerid string, queryString string, partition_key int) []map[string]interface{} {
 	endpoint := os.Getenv("Cosmos_DB_Endpoint")

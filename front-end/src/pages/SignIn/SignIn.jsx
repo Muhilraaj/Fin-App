43462fe5310f30264262656424dd5b5d3a3f0093
Muhil18 <muhil01803@gmail.com>
2023-10-12 20:58:25 +0530

    removed unwanted

--- front-end/src/pages/SignIn/SignIn.jsx
+++ front-end/src/pages/SignIn/SignIn.jsx
@@ -31,7 +31,6 @@ function Copyright(props) {
 }
 
 // TODO remove, this demo shouldn't need to reset the theme.
-const defaultTheme = createTheme();
 
 export default function SignIn() {
   const handleSubmit = async (event) => {

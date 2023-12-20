c712c193cb9a23c1c742cdd6b0b8eb6b9c40d086
Muhil18 <muhil01803@gmail.com>
2023-12-20 14:54:26 +0530

    update: page route change

--- front-end/src/pages/App/App.jsx
+++ front-end/src/pages/App/App.jsx
@@ -9,7 +9,7 @@ function App()
    return (
    <>
        <Routes>
-          <Route path="/page//login" element={<SignIn/>} />
+          <Route path="/page/login" element={<SignIn/>} />
           <Route path="/page/expense" element={<ExpenseForm/>} />
        </Routes>
    </>)

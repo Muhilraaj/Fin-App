f78dd27cf56c141d3dcbcac9f3f8ee77f2b9695c
Muhilraaj <muhil01803@gmail.com>
2025-11-18 20:50:18 +0530

    fix: proxy route

--- reverseProxy/proxy.go
+++ reverseProxy/proxy.go
@@ -57,6 +57,6 @@ func main() {
 	route.Any("/", frontendProxy)
 	route.GET("/static/*path", frontendProxy)
 	route.GET("/manifest.json", frontendProxy)
-	route.GET("/logo192.png", frontendProxy)
+	route.NoRoute(frontendProxy)
 	route.Run(":443")
 }
